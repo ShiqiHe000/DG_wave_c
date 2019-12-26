7124bd6779d4f4b8b46baf1edf32a33a643da6fc
Shiqi He <she010@uottawa.ca>
2019-12-25 22:18:15 -0500

    now can read quad_node number

--- src/dg_read_mesh_2d.cpp
+++ src/dg_read_mesh_2d.cpp
@@ -16,6 +16,7 @@ int Get_single_index(int row, int col, int total_col){
 
 }
 
+///
 void Read_mesh_2d(){
 
 	if(mpi::rank == 0){
@@ -29,10 +30,6 @@ void Read_mesh_2d(){
 		
 		// open the file
 		gmsh_file.open(fileinfo::fileplace);
-
-		if(gmsh_file.is_open()){
-			std::cout << "file open"<< "\n";
-		}
 
 		// store the information in a line here
 		std::string charline;
@@ -65,11 +62,62 @@ void Read_mesh_2d(){
 		
 		// allocate a memory space on heap to hold the 2d array
 		const int node_size = 2 * total_node; 
-		double* ptr_node;	// pointer to node coordinate
-		ptr_node = new double[node_size]; 	// store the node coordinates
-	
-		
+		double* node_xy;	// pointer to node coordinate
+		node_xy = new double[node_size]; 	// store the node coordinates
 
+		// read the node coordinates
+		for(int i = 0; i < total_node; ++i){
+			std::getline(gmsh_file, charline);
+			std::stringstream coord(charline);
+			
+			int nodei;
+
+			coord >> nodei >> node_xy[Get_single_index(i, 0, 2)]
+				>> node_xy[Get_single_index(i, 1, 2)];
+		
+		}	
+
+		// read dummy lines
+		std::getline(gmsh_file, charline);	// $EndNodes
+		std::getline(gmsh_file, charline);	// $Elements
+
+		// number of elements
+		std::getline(gmsh_file, charline);
+		std::stringstream elem(charline);
+		int num_of_element;
+		elem >> num_of_element;
+
+		// record 4 nodes number of each element
+		const int quad_size = 4 * num_of_element;
+		double* quad_node = new double[quad_size];
+		int total_quad{0};	// total quard-element number
+		
+		for(int i = 0; i < num_of_element; ++i){
+			std::getline(gmsh_file, charline);
+
+			int a, elem_type;	// get element type
+
+			std::stringstream type(charline);
+
+			type >> a >> elem_type;
+
+			if(elem_type == 3){
+
+				int b, c;
+
+				type >> a >> b >> c  
+					>> quad_node[Get_single_index(total_quad, 0, 4)] 
+					>> quad_node[Get_single_index(total_quad, 1, 4)]
+					>> quad_node[Get_single_index(total_quad, 2, 4)]
+					>> quad_node[Get_single_index(total_quad, 3, 4)];	
+				
+				++total_quad;
+			}
+
+		
+		}
+
+		
 //		int a;
 //		for(int i = 0; i < num_of_phy_name; i++){
 //			
