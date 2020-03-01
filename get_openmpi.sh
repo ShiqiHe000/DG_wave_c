165b674cbb255cffe40ffbf34dbaaedb0fa3c26b
ShiqiHe <she010@uottawa.ca>
2020-02-29 22:13:33 -0500

    t

--- get_openmpi.sh
+++ get_openmpi.sh
@@ -10,11 +10,11 @@ else
  rm openmpi-4.0.2.tar.gz
  echo "Configuring and building openmpi"
  cd openmpi-4.0.2
- ./configure --prefix=`pwd`/../openmpi
+ ./configure --prefix=`pwd`
  make -j 4 all
  make install
  cd ..
- rm -rf openmpi-4.0.2
+ rm openmpi-4.0.2.tar.gz
 fi
 
 test -n $CC && unset CC
