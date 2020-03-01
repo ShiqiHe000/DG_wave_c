9b1a74dd2a8dd5e2a33ef8dddc1e466cea8c8790
ShiqiHe <she010@uottawa.ca>
2020-03-01 08:52:32 -0500

    t

--- get_openmpi.sh
+++ get_openmpi.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 # check if OpenMPI is cached from previous build
-if [ -f openmpi/lib/libmpi.so]; then
+if [ -f openmpi/lib/libmpi.so ]; then
  echo "Using cached OpenMPI"
 else
  echo "Downloading OpenMPI source"
+ mkdir openmpi
  wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.2.tar.gz
  tar xfz openmpi-4.0.2.tar.gz
  rm openmpi-4.0.2.tar.gz
  echo "Configuring and building openmpi"
  cd openmpi-4.0.2
+ echo "the dir right now is ", `pwd`
  ./configure --prefix=`pwd`
  make -j 4 all
  make install
@@ -17,5 +19,5 @@ else
  rm openmpi-4.0.2.tar.gz
 fi
 
-test -n $CC && unset CC
-test -n $CXX && unset CXX
+#test -n $CC && unset CC
+#test -n $CXX && unset CXX
