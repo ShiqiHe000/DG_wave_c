7d0632390476ee21b92e147b3eee421e285856a7
ShiqiHe <she010@uottawa.ca>
2020-09-09 16:54:20 -0400

    keep working on external mirror image

--- src/dg_external_state.cpp
+++ src/dg_external_state.cpp
@@ -36,6 +36,25 @@ void External_state_Gaussian_exact(double t, double x, double y, std::vector<dou
 }
 
 /// @brief
+/// External state at bottom boundary comes from the mirror image wave. 
+void External_mirror_bottom(double t, double x, double y, std::vector<double>& q_ext, std::vector<int>& index){
+
+	
+
+	double inter = exp( - pow((user::kx * ( x - user::xx0) + 
+			user::ky * (y - user::yy0) - dg_fun::C * t), 2) / (user::D * user::D) );
+
+	q_ext[index[0]] = inter;
+
+	q_ext[index[1]] = user::kx / dg_fun::C * inter;
+
+	q_ext[index[2]] = user::ky / dg_fun::C * inter;
+
+}
+
+
+
+/// @brief
 /// Reflect boundary conditions. 
 void External_state_reflect_x(std::vector<double>& q_int, std::vector<double>& q_ext, 
 				std::vector<int>& index){
