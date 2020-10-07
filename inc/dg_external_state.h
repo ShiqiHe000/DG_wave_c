2593caaff41bca85d27a0b57fced9b7fa2a9748b
ShiqiHe <she010@uottawa.ca>
2020-10-07 19:51:51 -0400

    external state

--- inc/dg_external_state.h
+++ inc/dg_external_state.h
@@ -2,11 +2,27 @@
 #define DG_EXTERNAL_STATE_H
 
 #include <vector>
+#include <cmath>
+
+// global vars ---------------------------------------------------
+namespace mirror{
+	static const double kx = - sqrt(2.0) / 2.0;
+	static const double ky =   sqrt(2.0) / 2.0;
+	static const double D = 0.2 / (2.0 * sqrt(log(2.0)));
+	static const double x0 = 1.5;
+	static const double y0 = 0.5;	
+}
+//----------------------------------------------------------------
 
 void External_state_Gaussian_exact(double t, double x, double y, std::vector<double>& q_ext, std::vector<int>& index);
 
-void External_state_Gaussian_exact2(double t, double x, double y, std::vector<double>& q_ext, std::vector<int>& index);
-
 void External_state_sin_exact(double t, double x, double y, std::vector<double>& q_ext, std::vector<int>& index);
 
+void External_state_reflect_x(std::vector<double>& q_int, std::vector<double>& q_ext, 
+				std::vector<int>& index);
+
+void External_mirror_y_left(double t, double x, double y, std::vector<double>& q_ext, std::vector<int>& index);
+
+void External_state_reflect_y(std::vector<double>& q_int, std::vector<double>& q_ext, 
+				std::vector<int>& index);
 #endif
