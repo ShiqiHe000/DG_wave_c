6ad04871487cfe26122469830820b358dae3e986
ShiqiHe <she010@uottawa.ca>
2020-10-07 19:50:11 -0400

    reflecting B.C.

--- src/dg_external_state.cpp
+++ src/dg_external_state.cpp
@@ -3,18 +3,33 @@
 #include "dg_user_defined.h"
 #include <cmath>
 #include "dg_param.h"
-#include "dg_kx_ky.h"
+
+
+// forward declaration -------------------------------------------------------------------------------------------------
+void External_state_Gaussian_exact(double t, double x, double y, std::vector<double>& q_ext, std::vector<int>& index);
+
+void External_state_reflect_x(std::vector<double>& q_int, std::vector<double>& q_ext, 
+				std::vector<int>& index);
+
+void External_state_reflect_y(std::vector<double>& q_int, std::vector<double>& q_ext, 
+				std::vector<int>& index);
+
+void External_mirror_y_left(double t, double x, double y, std::vector<double>& q_ext, std::vector<int>& index);
+
+void External_state_sin_exact(double t, double x, double y, std::vector<double>& q_ext, std::vector<int>& index);
+//---------------------------------------------------------------------------------------------------------------------
 
 /// @brief
 /// Boundary conditions. User defined.
 /// @param t current time.
 /// @param x x coordinate (physical). 
 /// @param y y coordinate (physical). 
-/// @param 
+/// @param q_ext exrernal solutions (outputs). 
+/// @param index the index of three variables. 
 void External_state_Gaussian_exact(double t, double x, double y, std::vector<double>& q_ext, std::vector<int>& index){
 
 
-	double inter = exp( - pow((user::kx * (x - user::xx0) + 
+	double inter = exp( - pow((user::kx * ( x - user::xx0) + 
 			user::ky * (y - user::yy0) - dg_fun::C * t), 2) / (user::D * user::D) );
 
 	q_ext[index[0]] = inter;
@@ -24,23 +39,56 @@ void External_state_Gaussian_exact(double t, double x, double y, std::vector<dou
 	q_ext[index[2]] = user::ky / dg_fun::C * inter;
 }
 
-void External_state_Gaussian_exact2(double t, double x, double y, std::vector<double>& q_ext, std::vector<int>& index){
+/// @brief
+/// External state at bottom boundary comes from the mirror image wave.
+/// Note that the domain should be [0, 1] * [0, 1]. 
+/// The mirror image comes from [1, 2] * [0, 1]
+/// @param t current time.
+/// @param x x coordinate (physical). 
+/// @param y y coordinate (physical). 
+/// @param q_ext exrernal solutions (outputs). 
+/// @param index the index of three variables. 
+void External_mirror_y_left(double t, double x, double y, std::vector<double>& q_ext, std::vector<int>& index){
 
 
-	
-	double kx{}, ky{};
+	double inter = exp( - pow((mirror::kx * (x - mirror::x0) + 
+			mirror::ky * (y - mirror::y0) - dg_fun::C * t), 2) / (mirror::D * mirror::D) );
 
-	Get_kx_ky(kx, ky, t);
+	q_ext[index[0]] += inter;
 
-	double inter = exp( - pow((kx * (x - user::xx0) + 
-			ky * (y - user::yy0) - dg_fun::C * t), 2) / (user::D * user::D) );
+	q_ext[index[1]] += mirror::kx / dg_fun::C * inter;
 
-	q_ext[index[0]] = inter;
+	q_ext[index[2]] += mirror::ky / dg_fun::C * inter;
 
-	q_ext[index[1]] = kx / dg_fun::C * inter;
-
-	q_ext[index[2]] = ky / dg_fun::C * inter;
 }
+
+
+
+/// @brief
+/// Reflect boundary conditions. 
+void External_state_reflect_x(std::vector<double>& q_int, std::vector<double>& q_ext, 
+				std::vector<int>& index){
+
+	q_ext[index[0]] = q_int[index[0]];
+	
+	q_ext[index[1]] = (2.0 * q_int[index[0]] + dg_fun::C * q_int[index[1]]) / dg_fun::C;
+
+	q_ext[index[2]] = q_int[index[2]];
+
+}
+
+void External_state_reflect_y(std::vector<double>& q_int, std::vector<double>& q_ext, 
+				std::vector<int>& index){
+
+	q_ext[index[0]] = q_int[index[0]];
+
+	q_ext[index[1]] = 0.0;
+
+	q_ext[index[2]] = (2.0 * q_int[index[0]] + dg_fun::C * q_int[index[2]]) / dg_fun::C;
+
+
+}
+
 
 // test
 void External_state_sin_exact(double t, double x, double y, std::vector<double>& q_ext, std::vector<int>& index){
