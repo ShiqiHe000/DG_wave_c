78caba5429540d9da762523b9e37845b8f5badf7
Shiqi He <she010@uottawa.ca>
2020-10-18 09:51:19 -0400

    new routine to reinit the solution

---
+++ inc/dg_reinit.h
@@ -0,0 +1,6 @@
+#ifndef DG_REINIT_H
+#define DG_REINIT_H
+
+void DG_reinit(double t);
+
+#endif
---
+++ src/dg_reinit.cpp
@@ -0,0 +1,68 @@
+// Rewrite the solution on the mesh to be the exact solution. 
+
+#include "dg_local_storage.h"
+#include "dg_unit.h"
+#include "dg_param.h"
+#include "dg_affine_map.h"
+#include "dg_nodal_2d_storage.h"
+#include <cmath>	// exp
+#include "dg_single_index.h"
+#include "dg_user_defined.h"
+#include <iostream>	// test
+
+/// @brief
+/// @param t current time step
+void DG_reinit(double t){
+	
+	Unit* temp = local::head;
+
+	// traverse the linked list
+	for(int k = 0; k < local::local_elem_num; ++k){
+
+		// elemement size
+		double del_x = (temp -> xcoords[1]) - (temp -> xcoords[0]);
+		double del_y = (temp -> ycoords[1]) - (temp -> ycoords[0]);
+
+		for(int j = 0; j <= temp -> m; ++j){
+			
+			// map reference location to physical localtion
+			double gl_p_y = nodal::gl_points[temp -> m][j];
+			double y = Affine_mapping(gl_p_y, temp -> ycoords[0], del_y);
+			
+			for(int i = 0; i <= temp -> n; ++i){
+	
+				double gl_p_x = nodal::gl_points[temp -> n][i];
+				double x = Affine_mapping(gl_p_x, temp -> xcoords[0], del_x);
+			
+				int num_p = Get_single_index(i, j, temp -> m + 1);
+
+				// wave --------------------------------------------------------------------------------	
+				double inter = exp( - std::pow((user::kx * (x - user::xx0) + 
+							user::ky * (y - user::yy0) - dg_fun::C * t), 2) / std::pow(user::D, 2));
+      			
+				temp -> solution[0][num_p] = inter;
+				temp -> solution[1][num_p] = user::kx / dg_fun::C * inter;
+				temp -> solution[2][num_p] = user::ky / dg_fun::C * inter;
+				// -------------------------------------------------------------------------------------
+				
+
+				// test case sin(pi * x) + sin(pi * y) --------------------------------------------------
+//				double inter1 = std::sin(user::pi * x);
+//				double inter2 = std::sin(user::pi * y);
+//
+//				temp -> solution[0][num_p] = - 1.0 / user::pi * inter1 - 1.0 / user::pi * inter2;
+//				temp -> solution[1][num_p] = inter1;
+//				temp -> solution[2][num_p] = inter2;
+//				
+				//----------------------------------------------------------------------------------------
+
+			}
+		}
+	
+		temp = temp -> next;		
+
+	}
+
+
+}
+
